f256d6db8a808557343e7882451a3aeef0c0d934
Emily Elia <emily1997@gmail.com>
2019-09-28 00:51:56 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -1,75 +1,154 @@
-#!/usr/bin/env python
-
-import random
-import numpy as np
-from utils.treebank import StanfordSentiment
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+CS224N 2018-19: Homework 3
+run.py: Run the dependency parser.
+Sahil Chopra <schopra8@stanford.edu>
+"""
+from datetime import datetime
+import os
+import pickle
+import math
 import time
 
-from word2vec import *
-from sgd import *
+from torch import nn, optim
+import torch
+from tqdm import tqdm
 
-# Check Python Version
-import sys
-assert sys.version_info[0] == 3
-assert sys.version_info[1] >= 5
+from parser_model import ParserModel
+from utils.parser_utils import minibatches, load_and_preprocess_data, AverageMeter
 
-# Reset the random seed to make sure that everyone gets the same results
-random.seed(314)
-dataset = StanfordSentiment()
-tokens = dataset.tokens()
-nWords = len(tokens)
+# -----------------
+# Primary Functions
+# -----------------
+def train(parser, train_data, dev_data, output_path, batch_size=1024, n_epochs=10, lr=0.0005):
+    """ Train the neural dependency parser.
 
-# We are going to train 10-dimensional vectors for this assignment
-dimVectors = 10
+    @param parser (Parser): Neural Dependency Parser
+    @param train_data ():
+    @param dev_data ():
+    @param output_path (str): Path to which model weights and results are written.
+    @param batch_size (int): Number of examples in a single batch
+    @param n_epochs (int): Number of training epochs
+    @param lr (float): Learning rate
+    """
+    best_dev_UAS = 0
 
-# Context size
-C = 5
 
-# Reset the random seed to make sure that everyone gets the same results
-random.seed(31415)
-np.random.seed(9265)
+    ### YOUR CODE HERE (~2-7 lines)
+    ### TODO:
+    ###      1) Construct Adam Optimizer in variable `optimizer`
+    ###      2) Construct the Cross Entropy Loss Function in variable `loss_func`
+    ###
+    ### Hint: Use `parser.model.parameters()` to pass optimizer
+    ###       necessary parameters to tune.
+    ### Please see the following docs for support:
+    ###     Adam Optimizer: https://pytorch.org/docs/stable/optim.html
+    ###     Cross Entropy Loss: https://pytorch.org/docs/stable/nn.html#crossentropyloss
 
-startTime=time.time()
-wordVectors = np.concatenate(
-    ((np.random.rand(nWords, dimVectors) - 0.5) /
-       dimVectors, np.zeros((nWords, dimVectors))),
-    axis=0)
-wordVectors = sgd(
-    lambda vec: word2vec_sgd_wrapper(skipgram, tokens, vec, dataset, C,
-        negSamplingLossAndGradient),
-    wordVectors, 0.3, 40000, None, True, PRINT_EVERY=10)
-# Note that normalization is not called here. This is not a bug,
-# normalizing during training loses the notion of length.
 
-print("sanity check: cost at convergence should be around or below 10")
-print("training took %d seconds" % (time.time() - startTime))
+    ### END YOUR CODE
 
-# concatenate the input and output word vectors
-wordVectors = np.concatenate(
-    (wordVectors[:nWords,:], wordVectors[nWords:,:]),
-    axis=0)
+    for epoch in range(n_epochs):
+        print("Epoch {:} out of {:}".format(epoch + 1, n_epochs))
+        dev_UAS = train_for_epoch(parser, train_data, dev_data, optimizer, loss_func, batch_size)
+        if dev_UAS > best_dev_UAS:
+            best_dev_UAS = dev_UAS
+            print("New best dev UAS! Saving model.")
+            torch.save(parser.model.state_dict(), output_path)
+        print("")
 
-visualizeWords = [
-    "great", "cool", "brilliant", "wonderful", "well", "amazing",
-    "worth", "sweet", "enjoyable", "boring", "bad", "dumb",
-    "annoying", "female", "male", "queen", "king", "man", "woman", "rain", "snow",
-    "hail", "coffee", "tea"]
 
-visualizeIdx = [tokens[word] for word in visualizeWords]
-visualizeVecs = wordVectors[visualizeIdx, :]
-temp = (visualizeVecs - np.mean(visualizeVecs, axis=0))
-covariance = 1.0 / len(visualizeIdx) * temp.T.dot(temp)
-U,S,V = np.linalg.svd(covariance)
-coord = temp.dot(U[:,0:2])
+def train_for_epoch(parser, train_data, dev_data, optimizer, loss_func, batch_size):
+    """ Train the neural dependency parser for single epoch.
 
-for i in range(len(visualizeWords)):
-    plt.text(coord[i,0], coord[i,1], visualizeWords[i],
-        bbox=dict(facecolor='green', alpha=0.1))
+    Note: In PyTorch we can signify train versus test and automatically have
+    the Dropout Layer applied and removed, accordingly, by specifying
+    whether we are training, `model.train()`, or evaluating, `model.eval()`
 
-plt.xlim((np.min(coord[:,0]), np.max(coord[:,0])))
-plt.ylim((np.min(coord[:,1]), np.max(coord[:,1])))
+    @param parser (Parser): Neural Dependency Parser
+    @param train_data ():
+    @param dev_data ():
+    @param optimizer (nn.Optimizer): Adam Optimizer
+    @param loss_func (nn.CrossEntropyLoss): Cross Entropy Loss Function
+    @param batch_size (int): batch size
+    @param lr (float): learning rate
 
-plt.savefig('word_vectors.png')
+    @return dev_UAS (float): Unlabeled Attachment Score (UAS) for dev data
+    """
+    parser.model.train() # Places model in "train" mode, i.e. apply dropout layer
+    n_minibatches = math.ceil(len(train_data) / batch_size)
+    loss_meter = AverageMeter()
+
+    with tqdm(total=(n_minibatches)) as prog:
+        for i, (train_x, train_y) in enumerate(minibatches(train_data, batch_size)):
+            optimizer.zero_grad()   # remove any baggage in the optimizer
+            loss = 0. # store loss for this batch here
+            train_x = torch.from_numpy(train_x).long()
+            train_y = torch.from_numpy(train_y.nonzero()[1]).long()
+
+            ### YOUR CODE HERE (~5-10 lines)
+            ### TODO:
+            ###      1) Run train_x forward through model to produce `logits`
+            ###      2) Use the `loss_func` parameter to apply the PyTorch CrossEntropyLoss function.
+            ###         This will take `logits` and `train_y` as inputs. It will output the CrossEntropyLoss
+            ###         between softmax(`logits`) and `train_y`. Remember that softmax(`logits`)
+            ###         are the predictions (y^ from the PDF).
+            ###      3) Backprop losses
+            ###      4) Take step with the optimizer
+            ### Please see the following docs for support:
+            ###     Optimizer Step: https://pytorch.org/docs/stable/optim.html#optimizer-step
+
+
+            ### END YOUR CODE
+            prog.update(1)
+            loss_meter.update(loss.item())
+
+    print ("Average Train Loss: {}".format(loss_meter.avg))
+
+    print("Evaluating on dev set",)
+    parser.model.eval() # Places model in "eval" mode, i.e. don't apply dropout layer
+    dev_UAS, _ = parser.parse(dev_data)
+    print("- dev UAS: {:.2f}".format(dev_UAS * 100.0))
+    return dev_UAS
+
+
+if __name__ == "__main__":
+    # Note: Set debug to False, when training on entire corpus
+    debug = True
+    # debug = False
+
+    assert(torch.__version__ == "1.0.0"),  "Please install torch version 1.0.0"
+
+    print(80 * "=")
+    print("INITIALIZING")
+    print(80 * "=")
+    parser, embeddings, train_data, dev_data, test_data = load_and_preprocess_data(debug)
+
+    start = time.time()
+    model = ParserModel(embeddings)
+    parser.model = model
+    print("took {:.2f} seconds\n".format(time.time() - start))
+
+    print(80 * "=")
+    print("TRAINING")
+    print(80 * "=")
+    output_dir = "results/{:%Y%m%d_%H%M%S}/".format(datetime.now())
+    output_path = output_dir + "model.weights"
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    train(parser, train_data, dev_data, output_path, batch_size=1024, n_epochs=10, lr=0.0005)
+
+    if not debug:
+        print(80 * "=")
+        print("TESTING")
+        print(80 * "=")
+        print("Restoring the best model weights found on the dev set")
+        parser.model.load_state_dict(torch.load(output_path))
+        print("Final evaluation on test set",)
+        parser.model.eval()
+        UAS, dependencies = parser.parse(test_data)
+        print("- test UAS: {:.2f}".format(UAS * 100.0))
+        print("Done!")
